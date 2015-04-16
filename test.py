9971c61a1721519ba710ce7d33a602dfcd4f1740
Matthias Le Brun <mlbli@me.com>
2015-04-16 11:07:46 +0200

    used a separated method & added tests

--- test.py
+++ test.py
@@ -1,3 +1,17 @@
 from domcheck.strategies import *
 
 print check_html_file('localhost', 'valid', 'test')
+
+print search_meta_tag('<meta name="foo" content="bar"></head>', 'foo', 'bar') == True
+print search_meta_tag('<meta name="foo" content="bar"/></head>', 'foo', 'bar') == True
+print search_meta_tag('<meta name="foo"  content="bar"   /></head>', 'foo', 'bar') == True
+print search_meta_tag('<meta name=\'foo\' content=\'bar\'></head>', 'foo', 'bar') == True
+print search_meta_tag('<meta content="bar" name="foo"></head>', 'foo', 'bar') == True
+print search_meta_tag('<meta content=\'bar\' name=\'foo\'></head>', 'foo', 'bar') == True
+print search_meta_tag('</head><meta content=\'bar\' name=\'foo\'>', 'foo', 'bar') == False
+print search_meta_tag('</head><meta content=\'bar\' name=\'lol\'>', 'foo', 'bar') == False
+
+print search_meta_tag('<meta name="foo\' content="bar"></head>', 'foo', 'bar') == False
+print search_meta_tag('<meta name="foo" content=\'bar"></head>', 'foo', 'bar') == False
+print search_meta_tag('<meta content="bar\' content="foo"></head>', 'foo', 'bar') == False
+print search_meta_tag('<meta content="bar" content=\'foo"></head>', 'foo', 'bar') == False
