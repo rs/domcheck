bccfdf15fb441878227838a5e3b4d64b88eea200
Olivier Poitrey <rs@dailymotion.com>
2015-08-26 15:56:03 -0700

    Bump version to 1.1.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ else:
 
 setup(
     name='domcheck',
-    version='1.1.3',
+    version='1.1.4',
     description='Domain Ownership Checker',
     long_description=readme,
     author='Olivier Poitrey',
